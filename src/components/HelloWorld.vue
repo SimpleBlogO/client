00c1445ddae86206e637fd4352f766aa66eae949
gilbert <941321014@qq.com>
2019-12-08 23:48:12 +0800

    add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          Article
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          Column
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          Review
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Blog Manager App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HiWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    {{ info }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      info:null
+    }
+  },
+  mounted () {
+    this.$axios.get('http://192.168.100.134:8080/v1')
+      .then(response => (this.info = response.data))
+      .catch(function (error) { 
+        console.log(error);
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
